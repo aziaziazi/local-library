a7de806e3813372cfaed72f6b36538f13253c7e0
Camille Gabrieli <aziaziazi@gmail.com>
2020-10-22 15:41:51 +0200

    add routes

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-console.log('lala')
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'git push -u origin main';
+var mongoDB = 'mongodb+srv://testUser:testPassword@cluster0.5h0zh.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-console.log('lolo')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog')
 
 var app = express();
 
@@ -31,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
