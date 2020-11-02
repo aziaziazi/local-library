9ea29b441fbeafc2669dc2d574e053f58ed0d9bb
Camille Gabrieli <aziaziazi@gmail.com>
2020-11-02 16:32:15 +0100

    add compression and helmet middlewares

--- app.js
+++ app.js
@@ -15,13 +15,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog')
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
-// view engine setup
+app.use(helmet())
+
+  // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression({})); // compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
