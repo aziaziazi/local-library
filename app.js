1d91558c7b287ed08b7a19a6357f520cfd5777f0
Camille Gabrieli <aziaziazi@gmail.com>
2020-11-02 16:48:12 +0100

    prepare for heroku deployement

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://testUser:testPassword@cluster0.5h0zh.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://testUser:testPassword@cluster0.5h0zh.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
